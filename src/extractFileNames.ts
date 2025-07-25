d2d6e09c9a5924ecdaed04852e06f4fa43ab0339
Sepo <sepo@one.com>
2025-07-25 21:13:10 +0400

    task: Reset for mono

--- src/extractFileNames.ts
+++
@@ -1,5 +0,0 @@
-export const extractFileNames = (input: string): string => input
-  .trim()
-  .split('\n')
-  .map(path => path.split('/').reverse()[0])
-  .join('\n');
--- src/parseMdTable/constants.ts
+++
@@ -1,5 +0,0 @@
-export enum ParseMdTableOut {
-  STRUCT = 'struct',
-  CSV = 'csv',
-  CUSTOM_SEP = 'custom_sep',
-}
